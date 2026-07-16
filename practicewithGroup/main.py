3438a727c4576dfcd8f7a235fd5322d8633d8e92
Eibrahim Belayneh <abuabdellah1099@gmail.com>
2026-07-16 15:32:05 +0300

    day05: add SavingsAccount and CurrentAccount with polymorphic loop

--- practicewithGroup/main.py
+++ practicewithGroup/main.py
@@ -1,1 +1,13 @@
-name=""
+from array import array
+
+arr1 = array('i', [1, 2, 3, 4, 5])
+
+# Print each element
+# for n in arr1:
+#     print(n)
+
+# Print the overall length of the array (Outputs: 5)
+print(len(arr1))
+
+# print the type of the array (Outputs: <class 'array.array'>)
+print(type(arr1))
